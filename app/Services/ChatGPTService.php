b56814e4416f02ae586502f98876e7d371d48918
Angelica Aguilar <esangemari1@gmail.com>
2025-10-15 18:11:06 -0500

    Añadidos AuthServiceProvider y ChatGPTService, corregido AppServiceProvider

---
+++ app/Services/ChatGPTService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class ChatGPTService
+{
+    public function __construct()
+    {
+        // Código de inicialización si es necesario
+    }
+
+    public function enviarMensaje($mensaje)
+    {
+        // Lógica del servicio
+    }
+}
